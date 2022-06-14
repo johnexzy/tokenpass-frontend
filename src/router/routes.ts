56d0a17fa767b7b4650e7bf7a7ff60032d1fcbdd
iampapaisarkar <iampapaisarkar@gmail.com>
2022-06-14 22:03:56 +0530

    detail page design done

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('src/pages/HomePage.vue') }],
+    children: [
+      { path: '', component: () => import('src/pages/HomePage.vue') },
+      { path: '/detail', component: () => import('src/pages/DetailPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
