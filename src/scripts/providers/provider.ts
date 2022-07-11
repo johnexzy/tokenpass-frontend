681b3a3a4d5e126377087a54f2a51d672c3abe60
Johnexzy <obajohn75@gmail.com>
2022-07-11 21:35:37 +0100

    :sparkles: (added proxyaddresses to provide)

--- src/scripts/providers/provider.ts
+++ src/scripts/providers/provider.ts
@@ -2,15 +2,23 @@ import { providers } from 'ethers';
 import NetworkUtils from '../utils/networkUtils';
 export default class Provider {
   provider: providers.JsonRpcProvider;
+  network: string;
   constructor(network: string) {
+    this.network = network;
     this.provider = new providers.JsonRpcProvider(
       NetworkUtils.getRpcUrl(network)
     );
   }
 
+  getProxyAddress(): string {
+    return NetworkUtils.getProxyAddress(this.network);
+  }
+
   getProvider(): providers.JsonRpcProvider {
     return this.provider;
   }
+
+  // set provider to preffered
   setProvider(network: string) {
     this.provider = new providers.JsonRpcProvider(
       NetworkUtils.getRpcUrl(network)
