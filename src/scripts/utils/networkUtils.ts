7f4abcfbcdbec0a6bac1efce22d4b211dd47cdfe
Johnexzy <obajohn75@gmail.com>
2022-07-11 21:24:34 +0100

    added proxy address utils for network

--- src/scripts/utils/networkUtils.ts
+++ src/scripts/utils/networkUtils.ts
@@ -1,5 +1,6 @@
-import networks from "../seeds/network";
-import { NetworkUtilsType } from "../types/types";
+import networks from '../networks/network';
+import proxyAddress from '../networks/proxyAddress';
+import { NetworkUtilsType } from '../types/types';
 export default class NetworkUtils extends NetworkUtilsType {
   static getRpcUrl(network: string): string {
     const n = networks.find((r) => network === r.network)!;
@@ -8,5 +9,10 @@ export default class NetworkUtils extends NetworkUtilsType {
   static getNetwork(network: string) {
     const n = networks.find((r) => network === r.network);
     return n;
+  }
+
+  static getProxyAddress(network: string): string {
+    const n = proxyAddress.find((r) => network === r.network)!;
+    return n.address;
   }
 }
