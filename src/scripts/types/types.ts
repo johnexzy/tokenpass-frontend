91a54d2833d19b6804b646dbc16b55477fb8f71e
Johnexzy <obajohn75@gmail.com>
2022-07-11 21:16:21 +0100

    :label: (added types)

--- src/scripts/types/types.ts
+++ src/scripts/types/types.ts
@@ -4,6 +4,8 @@ import { TransactionResponse } from '@ethersproject/abstract-provider';
 export type NetworkType = {
   network: string;
   name: string;
+  label: string;
+  value: string;
   chainId: number;
   currency: string;
   rpc: string[];
@@ -47,3 +49,54 @@ export abstract class NetworkUtilsType {
   static getRpcUrl: (network: string) => string;
   static getNetwork: (network: string) => NetworkType | undefined;
 }
+
+export type tokenStandard = 'erc20' | 'erc721' | 'erc1155';
+export type tokenType<T, B> = {
+  blockchain: B;
+  token_standard: T;
+  contract_address: string;
+  token_id: string;
+  amount_required: string;
+  token_name: string;
+};
+export interface gatingFormData {
+  title: string;
+  type: string;
+  attachment: any;
+  description: string;
+  tagline: string;
+  gating: string;
+  creator_address: string;
+  subscription: {
+    allow: boolean;
+    price: number;
+    duration: number;
+  };
+  token: tokenType<tokenStandard, string>[];
+}
+export type tokenStandardOptions<T> = {
+  label: string;
+  value: T;
+};
+type gateType = {
+  contract_address: string;
+  token_standard: tokenStandard;
+  blockchain: string;
+  creator: string;
+  token_name: string;
+  token_requirements: {
+    token_id: number;
+    amount_required: number;
+  }[];
+};
+export type gatedResponseData = {
+  title: string;
+  slug: string;
+  type: string;
+  description: string | null;
+  tagline: string | null;
+  meta: null;
+  updated_at: string;
+  creator_address: string;
+  gate: gateType[];
+};
