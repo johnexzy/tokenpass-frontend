c23cb6feb6388d595f968aee2101b4f37fd38c68
Johnexzy <obajohn75@gmail.com>
2022-07-11 21:21:20 +0100

    :sparkles: (added proxyAddresses for different blockchain)

---
+++ src/scripts/networks/proxyAddress.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    network: 'mainnet',
+    address: '',
+  },
+  {
+    network: 'rinkeby',
+    address: '0x83Fe74d0A9F88D82416E922107417F3bF4C2bCA8',
+  },
+  {
+    network: 'polygon',
+    address: '',
+  },
+  {
+    network: 'bsc',
+    address: '',
+  },
+] as { network: string; address: string }[];
